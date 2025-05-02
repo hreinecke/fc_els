b3b0bcc22f8b793c02411576e414c4101bf5ef24
Hannes Reinecke <hare@suse.de>
2025-05-02 10:28:11 +0200

    Simplify san_nswalk

--- san_nswalk.c
+++ san_nswalk.c
@@ -1,23 +1,7 @@
 /*
  * Copyright(c) 2013 Hannes Reinecke, SUSE Linux Products GmbH
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
-
-/*
- * san_resync - resynchronize SAN port states
+ * san_nswalk - get portnames from unzoned server
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,6 +32,7 @@ typedef uint8_t u8;
 #include <scsi/sg.h>
 
 #include "fc_ns.h"
+#include "fc_ms.h"
 #include "fc_gs.h"
 #include "fc_els.h"
 #include "scsi_bsg_fc.h"
@@ -72,6 +57,7 @@ static const char *cmdname = "san_resync";
 
 #define FC_WKA_FABRIC_CONTROLLER ((fc_fid_t)0xfffffd)
 #define FC_WKA_DIRECTORY_SERVICE ((fc_fid_t)0xfffffc)
+#define FC_WKA_UNZONED_NAME_SERVER ((fc_fid_t)0xfffffa)
 
 static int els_timeout = DEF_ELS_TIMEOUT;
 static int fp_hba = -1;	/* number of fc_host to be used */
@@ -278,6 +264,118 @@ fp_find_did(int hba_num, fc_fid_t did)
 	}
 	closedir(dirp);
 	return rport_num;
+}
+
+/*
+ * Query unzoned name server.
+ */
+static int
+fp_ns_get_device_list(int hba_num, uint32_t op, fc_wwn_t wwn,
+	     char *response, size_t *resp_len)
+{
+	char bsg_dev[80];
+	int wka_port = -1;
+	struct ct_get_id {
+		struct fc_ct_hdr hdr;
+		uint8_t	 wwn[8];
+	} ct;
+	struct fc_bsg_request cdb;
+	struct fc_bsg_reply reply;
+	struct sg_io_v4 sg_io;
+	size_t actual_len;
+	int fp_rport_fd;
+	int cmd, rc = 0;
+
+	wka_port = fp_find_did(hba_num, FC_WKA_UNZONED_NAME_SERVER);
+	if (wka_port < 0) {
+		fprintf(stderr, "host%d: No remote port found for WKA %06lx\n",
+		       hba_num, (unsigned long)FC_WKA_UNZONED_NAME_SERVER);
+		system_errors++;
+		return ENXIO;
+	}
+
+	sprintf(bsg_dev, "/dev/bsg/rport-%d:0-%d", hba_num, wka_port);
+	fp_rport_fd = open(bsg_dev, O_RDWR);
+	if (fp_rport_fd < 0) {
+		fprintf(stderr, "host%d: Cannot open bsg device %s: %s\n",
+			hba_num, bsg_dev, strerror(errno));
+		system_errors++;
+		return errno;
+	}
+	memset((char *)&cdb, 0, sizeof(cdb));
+	memset(&ct, 0, sizeof(ct));
+	ct.hdr.ct_rev = FC_CT_REV;
+	ct.hdr.ct_fs_type = FC_FST_DIR;
+	ct.hdr.ct_fs_subtype = FC_NS_SUBTYPE;
+	ct.hdr.ct_options = 0;
+	ct.hdr.ct_cmd = htons(op);
+	ct.hdr.ct_mr_size = *resp_len;
+
+	hton64(ct.wwn, wwn);
+
+	cdb.msgcode = FC_BSG_RPT_CT;
+	memcpy(&cdb.rqst_data.r_ct.preamble_word0, &ct.hdr,
+	       3 * sizeof(uint32_t));
+
+	sg_io.guard = 'Q';
+	sg_io.protocol = BSG_PROTOCOL_SCSI;
+	sg_io.subprotocol = BSG_SUB_PROTOCOL_SCSI_TRANSPORT;
+	sg_io.request_len = sizeof(cdb);
+	sg_io.request = (uintptr_t)&cdb;
+	sg_io.dout_xfer_len = sizeof(ct);
+	sg_io.dout_xferp = (uintptr_t)&ct;
+	sg_io.din_xfer_len = *resp_len;
+	sg_io.din_xferp = (uintptr_t)response;
+	sg_io.max_response_len = sizeof(reply);
+	sg_io.response = (uintptr_t)&reply;
+	sg_io.timeout = 1000;	/* millisecond */
+	memset(&reply, 0, sizeof(reply));
+	memset(response, 0, *resp_len);
+
+	rc = ioctl(fp_rport_fd, SG_IO, &sg_io);
+	if (rc < 0) {
+		fprintf(stderr, "host%d: %s error; %s\n", hba_num,
+			op == FC_NS_GID_PN ? "GID_PN" : "GID_NN",
+			strerror(errno));
+		close(fp_rport_fd);
+		system_errors++;
+		return errno;
+	}
+
+	cmd = ((response[8]<<8) | response[9]) & 0xffff;
+	if (cmd == FC_FS_RJT) {
+		if (response[13] == FC_FS_RJT_UNABL &&
+		    (response[14] == FC_FS_EXP_PNAM ||
+		     response[14] == FC_FS_EXP_NNAM)) {
+			fprintf(stderr, "host%d: %s rejected, "
+				"not registered\n", hba_num,
+				op == FC_NS_GID_PN ? "GID_PN" : "GID_NN");
+			port_errors++;
+		} else {
+			fprintf(stderr, "host%d: %s rejected, "
+				"reason %02x/%02x\n", hba_num,
+				op == FC_NS_GID_PN ? "GID_PN" : "GID_NN",
+				response[13], response[14]);
+			els_errors++;
+		}
+		close(fp_rport_fd);
+		*resp_len = 0;
+		return 0;
+	} else if (cmd != FC_FS_ACC) {
+		fprintf(stderr, "host%d: %s result %x\n", hba_num,
+			op == FC_NS_GID_PN ? "GID_PN" : "GID_NN", cmd);
+		close(fp_rport_fd);
+		*resp_len = 0;
+		els_errors++;
+		return 0;
+	}
+
+	actual_len = reply.reply_payload_rcv_len;
+	if (actual_len < *resp_len)
+		*resp_len = actual_len;
+
+	close(fp_rport_fd);
+	return 0;
 }
 
 /*
@@ -507,91 +605,6 @@ fp_lookup_target_by_wwnn(int hba_num, unsigned long long wwnn)
 	return did;
 }
 
-/*
- * ELS_ECHO request format being used.
- * Put a sequence number in the payload, followed by the pattern.
- */
-struct fcping_echo {
-	uint8_t		fe_op;		/* opcode */
-	uint8_t		fe_resvd[3];	/* reserved, must be zero */
-	uint32_t	fe_seq;		/* sequence number */
-};
-
-/*
- * Setup buffer to be sent.
- */
-struct fcping_echo *
-fp_buf_setup(int buf_len)
-{
-	struct fcping_echo *ep;
-	uint8_t *pp;
-	int len;
-	int i;
-
-	/*
-	 * Alloc extra in case of odd len or shorter than minimum.
-	 */
-	len = buf_len + sizeof(*ep) + sizeof(uint32_t);
-	ep = calloc(1, len);
-	if (ep == NULL)
-		return NULL;
-	ep->fe_op = ELS_ECHO;
-	ep->fe_seq = htonl(1);
-	i = 0;
-	for (pp = (uint8_t *) (ep + 1); pp < (uint8_t *) ep + buf_len; pp++)
-		*pp = i++;
-	return ep;
-}
-
-static int
-send_els_echo(int hba_num, int rport, void *fp_buf, uint32_t fp_len,
-	      unsigned char *resp, uint32_t *resp_len)
-{
-	struct fc_bsg_request cdb;
-	char bsg_dev[256];
-	char sense[MAX_SENSE_LEN];
-	struct sg_io_v4 sg_io;
-	int fp_rport_fd, rc;
-
-	sprintf(bsg_dev, "/dev/bsg/rport-%d:0-%d", hba_num, rport);
-	fp_rport_fd = open(bsg_dev, O_RDWR);
-	if (fp_rport_fd < 0) {
-		fprintf(stderr, "rport-%d:0-%d: cannot open bsg %s: %m\n",
-			hba_num, rport, bsg_dev);
-		system_errors++;
-		return ENOENT;
-	}
-	cdb.msgcode = FC_BSG_RPT_ELS;
-	cdb.rqst_data.h_els.command_code = ELS_ECHO;
-
-	sg_io.guard = 'Q';
-	sg_io.protocol = BSG_PROTOCOL_SCSI;
-	sg_io.subprotocol = BSG_SUB_PROTOCOL_SCSI_TRANSPORT;
-	sg_io.request_len = sizeof(cdb);
-	sg_io.request = (unsigned long)&cdb;
-	sg_io.dout_xfer_len = fp_len;
-	sg_io.dout_xferp = (unsigned long)fp_buf;
-	sg_io.din_xfer_len = *resp_len;
-	sg_io.din_xferp = (unsigned long)resp;
-	sg_io.max_response_len = sizeof(sense);
-	sg_io.response = (unsigned long)sense;
-	sg_io.timeout = (unsigned long)els_timeout * 1000;
-	memset(sense, 0, sizeof(sense));
-
-	rc = ioctl(fp_rport_fd, SG_IO, &sg_io);
-	if (rc >= 0) {
-		*resp_len = sg_io.din_xfer_len - sg_io.din_resid;
-		rc = 0;
-	} else {
-		fprintf(stderr, "rport-%d:0-%d: bsg ioctl failed: %m\n",
-			hba_num, rport);
-		system_errors++;
-		rc = -errno;
-	}
-	close(fp_rport_fd);
-	return rc;
-}
-
 struct rport_type_t {
 	int hba;
 	int rport;
@@ -637,129 +650,6 @@ fp_lookup_next_port(int hba_num, int fd, fc_fid_t start_did,
 	return rc;
 }
 
-int rport_compare (const void *pa, const void *pb)
-{
-	const struct rport_type_t *rpa = pa;
-	const struct rport_type_t *rpb = pb;
-
-	if (rpa->did < rpb->did)
-		return -1;
-	if (rpa->did > rpb->did)
-		return 1;
-	return 0;
-}
-
-void rport_check(struct rport_type_t *rp)
-{
-	char port_name[64], port_state[64];
-	ssize_t len;
-	int rc, retry = 5;
-	fc_fid_t did;
-
-	if (rp->rport == -1)
-		sprintf(port_name,"rport-%d:0-X", rp->hba);
-	else
-		sprintf(port_name,"rport-%d:0-%d", rp->hba, rp->rport);
-
-	if (verbose)
-		printf("%s: DID %06x WWPN 0x%08" PRIx64 " WWNN 0x%08" PRIx64 "\n",
-		       port_name, rp->did, rp->wwpn, rp->wwnn);
-	if (FCID_IS_WKA(rp->did))
-		return;
-
-	if (rp->rport == -1) {
-		if (verbose)
-			printf("\tState: removed, registered\n");
-		port_errors++;
-		return;
-	}
-	memset(port_state, 0x0, sizeof(port_state));
-	rc = fc_rport_get_attr(port_name, "port_state",
-			       port_state, sizeof(port_state));
-	if (rc < 0) {
-		if (verbose)
-			printf("\tState: unknown\n");
-		port_errors++;
-		return;
-	}
-	len = strlen(port_state);
-	if (port_state[len - 1] == '\n')
-		port_state[len - 1] = '\0';
-	if (verbose)
-		printf("\tState: %s", port_state);
-	if (strcmp(port_state, "Online")) {
-		/*
-		 * Port is not online, so internal state machine
-		 * has detected an issue. No error.
-		 */
-		if (verbose)
-			printf("\n");
-		return;
-	}
-
-	did = fp_lookup_target_by_wwpn(fp_hba, rp->wwpn);
-	if (did && did == rp->did) {
-		if (verbose)
-			printf(", registered");
-	} else {
-		if (verbose)
-			printf(", unregistered\n");
-		if (did)
-			port_errors++;
-		return;
-	}
-retry:
-	rc = fp_send_ping(fp_hba, rp->rport, rp->did);
-	if (rc < 0) {
-		if (verbose)
-			printf(", failed to send ELS ECHO, error %d\n", -rc);
-		port_errors++;
-	} else {
-		switch (rc) {
-		case 0:
-			if (verbose)
-				printf(", alive\n");
-			break;
-		case ENODATA:
-			if (--retry)
-				goto retry;
-			if (verbose)
-				printf(", ELS ECHO buffer mismatch\n");
-			break;
-		case EAGAIN:
-			if (verbose)
-				printf(", ELS ECHO rejected\n");
-			port_errors++;
-			break;
-		default:
-			if (verbose)
-				printf(", ELS ECHO error %d\n", rc);
-			port_errors++;
-			break;
-		}
-	}
-}
-
-void rport_print(const void *nodep, const VISIT which, const int depth)
-{
-	struct rport_type_t *rp;
-
-	switch (which) {
-	case preorder:
-		break;
-	case postorder:
-		rp = *(struct rport_type_t **)nodep;
-		rport_check(rp);
-		break;
-	case endorder:
-		break;
-	case leaf:
-		rp = *(struct rport_type_t **)nodep;
-		rport_check(rp);
-		break;
-	}
-}
-
 static int
 walk_ns(int hba_num, fc_fid_t hba_did)
 {
@@ -790,19 +680,6 @@ walk_ns(int hba_num, fc_fid_t hba_did)
 	rport = malloc(sizeof(struct rport_type_t));
 	memset(rport, 0x0, sizeof(struct rport_type_t));
 	rc = fp_lookup_next_port(fp_hba, wka_fd, hba_did, rport);
-	while (rc == 0 && rport->did && rport->did != hba_did) {
-		did = rport->did;
-		ptr = tsearch((void *)rport, &root, rport_compare);
-		if (ptr == NULL) {
-			fprintf(stderr, "host%d: Failed to insert did %06x\n",
-				hba_num, did);
-			system_errors++;
-		} else if ((struct rport_type_t *)ptr == rport) {
-			rport = malloc(sizeof(struct rport_type_t));
-			memset(rport, 0x0, sizeof(struct rport_type_t));
-		}
-		rc = fp_lookup_next_port(fp_hba, wka_fd, did, rport);
-	}
 	free(rport);
 	close(wka_fd);
 	return rc;
@@ -827,16 +704,5 @@ int main(int argc, char *argv[])
 
 	walk_ns(fp_hba, fp_hba_did);
 
-	twalk(root, rport_print);
-	tdestroy(root, free);
-
-	printf("host %d: %d System errors, %d ELS errors, %d port mismatches\n",
-	       fp_hba, system_errors, els_errors, port_errors);
-	if (port_errors)
-		rc |= 1;
-	if (els_errors)
-		rc |= 2;
-	if (system_errors)
-		rc |= 4;
 	return rc;
 }
